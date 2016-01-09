aed09c2860825c1f46f934c999100e772c9c3616
EdKung <edkung0728@gmail.com>
2016-01-09 12:26:01 +0800

    fix conversion from xml to JSON problem

--- src/back_end/DataCrawler.js
+++ src/back_end/DataCrawler.js
@@ -1,10 +1,9 @@
 var express = require('express');
 var request = require('request');
 var parser = require('xml2json');
-var jsonfile = require('jsonfile')
+var util = require('util');
 var fs = require('fs');
 var url = 'http://data.taipei/opendata/datalist/datasetMeta/download;jsessionid=6CB2E55BF3850495121F0DB94610C2AC?id=5e4db75d-734e-42b7-8284-df413aa8122a&rid=bf0f5218-73a7-429b-ae30-3a87ab818927';
-var app = express();
 
 crawler();
 
@@ -14,6 +13,8 @@ function crawler() {
   var jsonPath = './busker.json';
   var source = request(url);
   var downloadFile = fs.createWriteStream(xmlPath);
+  var input = fs.createReadStream('./busker.xml');
+  var output = fs.createWriteStream('./busker.json');
 
   source.on('response', function(res) {
     res.pipe(downloadFile);
@@ -21,10 +22,8 @@ function crawler() {
 
   downloadFile.on('finish', function() {
     fs.readFile(xmlPath, {encoding: 'utf-8'}, function(err, data){
-      var json = parser.toJson(data);
-      jsonfile.writeFile(jsonPath, json, function (err) {
-        console.error(err)
-      })
+      var jsonObj = parser.toJson(data);
+      fs.writeFileSync(jsonPath, jsonObj, 'utf-8');
     });
   });
 }
