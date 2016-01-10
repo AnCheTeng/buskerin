1b36c390e4f4cc855f3440dd8bc92cc42c2c4d10
An-Che Teng <angeldan0627@gmail.com>
2016-01-10 21:28:25 +0800

    Fix data crawler

--- src/back_end/DataCrawler.js
+++ src/back_end/DataCrawler.js
@@ -24,45 +24,46 @@ function crawler() {
   });
 
   downloadFile.on('finish', function() {
-    Busker.remove({}, function(){});
-    fs.readFile(xmlPath, {encoding: 'utf-8'}, function(err, data){
+    Busker.remove({}, function() {});
+    fs.readFile(xmlPath, {
+      encoding: 'utf-8'
+    }, function(err, data) {
       var jsonObj = parser.toJson(data);
       var buskerList = JSON.parse(jsonObj);
-      var save_array=[];
+      var raw_array = buskerList.datas.Performer;
       // i < buskerList.datas.Performer.length
-      for (var i = 0; i < buskerList.datas.Performer.length; i++) {
+      raw_array = ObjectArrayUnique(raw_array, "performer_no");
+      for (var i = 0, len = raw_array.length; i < len; i++) {
 
-        var busker = buskerList.datas.Performer[i];
-        Busker.findOne({"num":busker.performer_no}).exec(function(err, duplicate){
-          cnosole.log(duplicate);
-          if(duplicate==undefined){
-            refineData(busker);
-            // Create a new instance of Busker
-            var newMember = new Busker({
-              num: busker.performer_no,
-              group_name: busker.group_name,
-              performer_name: busker.performer_name,
-              perform_type: busker.perform_type,
-              perform_content: busker.perform_content,
-              email: busker.email,
-              img: "https://pbs.twimg.com/profile_images/557472607985876992/2RF6SFTW.jpeg",
-              lat: "",
-              long: "",
-              time_stamp: +new Date()
-            });
-            // newMember.save(function(err){
-            //   if(err){
-            //     console.error(err);
-            //   }
-            // });
-            save_array.push(newMember);
-            save_all(save_array);
-            logger(newMember);
+        var busker = raw_array[i];
+
+        refineData(busker);
+        // Create a new instance of Busker
+        var newMember = new Busker({
+          num: busker.performer_no,
+          group_name: busker.group_name,
+          performer_name: busker.performer_name,
+          perform_type: busker.perform_type,
+          perform_content: busker.perform_content,
+          email: busker.email,
+          img: "https://pbs.twimg.com/profile_images/557472607985876992/2RF6SFTW.jpeg",
+          lat: "",
+          long: "",
+          time_stamp: +new Date()
+        });
+        newMember.save(function(err) {
+          if (err) {
+            console.error(err);
           }
         });
+
+        logger(newMember);
+
       }
-      fs.writeFile(jsonPath, jsonObj, {encoding: 'utf-8'}, function(err){
-        if(err) {
+      fs.writeFile(jsonPath, jsonObj, {
+        encoding: 'utf-8'
+      }, function(err) {
+        if (err) {
           console.error(err);
         }
       });
@@ -75,42 +76,42 @@ var hasOwnProperty = Object.prototype.hasOwnProperty;
 
 function isEmpty(obj) {
 
-    // null and undefined are "empty"
-    if (obj == null) return true;
-    if (obj == undefined) return true;
+  // null and undefined are "empty"
+  if (obj == null) return true;
+  if (obj == undefined) return true;
 
-    // Assume if it has a length property with a non-zero value
-    // that that property is correct.
-    if (obj.length > 0)    return false;
-    if (obj.length === 0)  return true;
+  // Assume if it has a length property with a non-zero value
+  // that that property is correct.
+  if (obj.length > 0) return false;
+  if (obj.length === 0) return true;
 
-    // Otherwise, does it have any properties of its own?
-    // Note that this doesn't handle
-    // toString and valueOf enumeration bugs in IE < 9
-    for (var key in obj) {
-        if (hasOwnProperty.call(obj, key)) return false;
-    }
+  // Otherwise, does it have any properties of its own?
+  // Note that this doesn't handle
+  // toString and valueOf enumeration bugs in IE < 9
+  for (var key in obj) {
+    if (hasOwnProperty.call(obj, key)) return false;
+  }
 
-    return true;
+  return true;
 }
 
 function refineData(busker) {
   // Pre-assign those empty value
-  if(isEmpty(busker.group_name) && isEmpty(busker.performer_name)) {
+  if (isEmpty(busker.group_name) && isEmpty(busker.performer_name)) {
     // console.log('Both names are empty');
     busker.group_name = 'Who are you';
     busker.performer_name = 'Who are you';
-  } else if(isEmpty(busker.group_name) && !isEmpty(busker.performer_name)) {
+  } else if (isEmpty(busker.group_name) && !isEmpty(busker.performer_name)) {
     // console.log('group_name is empty');
-      busker.group_name = busker.performer_name;
-  } else if(!isEmpty(busker.group_name) && isEmpty(busker.performer_name)) {
-      // console.log('performer_name is empty');
-      busker.performer_name = busker.group_name;
+    busker.group_name = busker.performer_name;
+  } else if (!isEmpty(busker.group_name) && isEmpty(busker.performer_name)) {
+    // console.log('performer_name is empty');
+    busker.performer_name = busker.group_name;
   }
-  if(isEmpty(busker.perform_content)) {
+  if (isEmpty(busker.perform_content)) {
     busker.perform_content = 'None';
   }
-  if(isEmpty(busker.email)) {
+  if (isEmpty(busker.email)) {
     busker.email = 'None';
   }
 }
@@ -132,10 +133,29 @@ function logger(newMember) {
   // console.log('perform_content: ' + typeof newMember.perform_content);
 }
 
-function save_all(save_array){
-  if(save_array.length!=0){
-    save_array.shift().save(function(err,saved){
-      save_all(save_array);
-    })
+function ObjectArrayIndexOf(myArray, searchTerm, property) {
+  for (var i = 0, len = myArray.length; i < len; i++) {
+    if (myArray[i][property] === searchTerm) return i;
   }
+  return -1;
+}
+
+function ObjectArrayUnique(myArray, uniqProperty) {
+
+  var result = [];
+  var duplicate = 0;
+
+  while(myArray.length!=0){
+    var uniq_elem = myArray.shift();
+
+    duplicate = ObjectArrayIndexOf(myArray, uniq_elem[uniqProperty], uniqProperty);
+
+    while(duplicate>=0) {
+      myArray.splice(duplicate, 1);
+      duplicate = ObjectArrayIndexOf(myArray, uniq_elem[uniqProperty], uniqProperty);
+    }
+    result.push(uniq_elem);
+  }
+
+  return result;
 }
