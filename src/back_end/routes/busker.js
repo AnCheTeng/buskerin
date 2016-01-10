913e8bf633ffb166c9dc361feac19c7cdc2b9fb9
EdKung <edkung0728@gmail.com>
2016-01-10 10:09:00 +0800

    Implement get method to get all the data of buskers

--- src/back_end/routes/busker.js
+++ src/back_end/routes/busker.js
@@ -1,49 +1,35 @@
 var express = require('express');
 var bodyParser = require('body-parser');
 var mongoose = require('mongoose');
-var Busker = require('./Model_Busker');
-var Member = require('./Model_Member');
+var Busker = require('../Model_Busker');
+var Member = require('../Model_Member');
 
 var router = express.Router();
 var parseUrlencoded = bodyParser.urlencoded({extended:false});
 
-mongoose.connect('mongodb://localhost/busker');
-
-var buskers = {
-  'Slow motion': 'To act like a robot',
-  'Ball magic': 'Magical balls',
-  'Kong Fu': 'Do some easy steps to interact with people'
-};
+mongoose.createConnection('mongodb://localhost/busker');
 
 router.route('/')
   .get(function(request, response) {
-    response.json(Object.keys(buskers));
+    Busker.find().exec(function(err, foundData) {
+      response.send(foundData);
+    });
   })
   .post(parseUrlencoded, function(request, response){
     var newBusker = request.body;
-    buskers[newBusker.name] = newBusker.description;
-    response.status(201).json(newBusker.name);
-  });
-
-
-router.route('/:name')
-  .all(function (request, response, next){
-    var name = request.params.name;
-    var Busker = name[0].toUpperCase() + name.slice(1).toLowerCase();
-    request.pName = Busker;
-    next();
-  })
-  .delete(function(request, response) {
-    delete buskers[request.pName];
-    response.sendStatus(200);
-  })
-  .get(function(request, response) {
-    var description = buskers[request.pName];
-    if (!description) {
-      response.status(404).json('No description is ' + request.params.name);
-    } else {
-      response.json(description);
-    }
+    var newMember = new Busker({
+      num: 'TODO',
+      group_name: 'TODO',
+      performer_name: 'TODO',
+      perform_type: 'TODO',
+      perform_content: 'TODO'
+    });
+    newMember.save(function(err){
+      if(err){
+        console.error(err);
+      }
+    });
+    response.status(201).json(newMember);
   });
 
 module.exports = router;
