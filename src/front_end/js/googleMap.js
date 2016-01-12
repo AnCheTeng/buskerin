403185fd084fd2a80228ad139bd784681213b000
EdKung <edkung0728@gmail.com>
2016-01-12 17:28:28 +0800

    Implement add multiple markers

--- src/front_end/js/googleMap.js
+++ src/front_end/js/googleMap.js
@@ -1,82 +1,123 @@
-var myLoc = {lat: 25, lng: 121.4};
+var myLoc = {lat: 25.019818307021946, lng: 121.54214659134442};
 var markers = [];
 var map;
+var infoWindowww;
 
 function initMap() {
 
-  map = new google.maps.Map(document.getElementById('map'), {
-    zoom: 12,
-    center: myLoc
-  });
+  var locations = [
+       ['A', 25, 121.4, 4],
+       ['B', 25, 121.41, 5],
+       ['C', 25, 121.43, 3],
+       ['D', 25, 121.47, 2],
+       ['E', 25, 121.5, 1]
+     ];
 
-  // Add a marker to toggle Bounce
-  markers.push(new google.maps.Marker({
-    position: {lat: 24.9, lng: 121.4},
-    map: map,
-    animation: google.maps.Animation.DROP,
-    title: 'Click to zoom'
-  }).addListener('click', toggleBounce));
+   var map = new google.maps.Map(document.getElementById('map'), {
+     zoom: 10,
+     center: new google.maps.LatLng(25.019818307021946, 121.54214659134442),
+     mapTypeId: google.maps.MapTypeId.ROADMAP
+   });
 
-  // Show the position of the user using this webpage
+   var infowindow = new google.maps.InfoWindow();
+
+   var marker, i;
+
+   for (i = 0; i < locations.length; i++) {
+     marker = new google.maps.Marker({
+       position: new google.maps.LatLng(locations[i][1], locations[i][2]),
+       map: map
+     });
+
+     google.maps.event.addListener(marker, 'click', (function(marker, i) {
+       return function() {
+         infowindow.setContent(locations[i][0]);
+         infowindow.open(map, marker);
+       }
+     })(marker, i));
+   }
+
+  // // Show the position of the user using this webpage
   if (navigator.geolocation) {
     navigator.geolocation.getCurrentPosition(function(position) {
-      markers.push(new google.maps.Marker({
-        position: {lat: position.coords.latitude, lng: position.coords.longitude},
-        map: map,
-        title: 'Click to zoom'
-      }).addListener('click', function() {
-        map.setZoom(12);
-        map.setCenter(this.getPosition());
-        infoWindow.open(map, this);
-      }));
+      // map.setCenter({lat: position.coords.latitude, lng: position.coords.longitude});
+      marker = new google.maps.Marker({
+        position: new google.maps.LatLng(position.coords.latitude, position.coords.longitude),
+        map: map
+      });
+
+      google.maps.event.addListener(marker, 'click', (function(marker, i) {
+        return function() {
+          infowindow.setContent('NTU');
+          infowindow.open(map, marker);
+        }
+      })(marker, i));
     });
   } else {
     // Browser doesn't support Geolocation
     handleLocationError(false, infoWindow, map.getCenter());
   }
 
-  // Construct a new InfoWindow.
-  var infoWindow = new google.maps.InfoWindow({
-    content: 'National Taiwan University'
-  });
+  // Add a marker to toggle Bounce
+  // markers.push(new google.maps.Marker({
+  //   position: {lat: 25, lng: 121.5},
+  //   map: map,
+  //   animation: google.maps.Animation.DROP,
+  //   title: 'Click to zoom'
+  // }).addListener('click', toggleBounce));
+}
 
-  function toggleBounce() {
-    if (this.getAnimation() !== null) {
-      this.setAnimation(null);
-    } else {
-      this.setAnimation(google.maps.Animation.BOUNCE);
-    }
-  }
-
-  function handleLocationError(browserHasGeolocation, infoWindow, pos) {
-    infoWindow.setPosition(pos);
-    infoWindow.setContent(browserHasGeolocation ?
-                          'Error: The Geolocation service failed.' :
-                          'Error: Your browser doesn\'t support geolocation.');
-  }
-
-  function drop() {
-    clearMarkers();
-    for (var i = 0; i < neighborhoods.length; i++) {
-      addMarkerWithTimeout(neighborhoods[i], i * 200);
-    }
-  }
-
-  function addMarkerWithTimeout(position, timeout) {
-    window.setTimeout(function() {
-      markers.push(new google.maps.Marker({
-        position: position,
-        map: map,
-        animation: google.maps.Animation.DROP
-      }));
-    }, timeout);
-  }
-
-  // Clear all the markers
-  function clearMarkers() {
-    for (var i = 0; i < markers.length; i++) {
-      markers[i].setMap(null);
-    }
-    markers = [];
+function createInfoWindowForBuskers(){
+  var i, lengt=10;
+  for(i=0; i<length; i++) {
+    infoWindows[i] = createInfoWindow(i);
   }
 }
+
+function createInfoWindow(information){
+  return new google.maps.InfoWindow({
+    content: information
+  });
+}
+
+function toggleBounce() {
+  if (this.getAnimation() !== null) {
+    this.setAnimation(null);
+  } else {
+    this.setAnimation(google.maps.Animation.BOUNCE);
+  }
+}
+
+function handleLocationError(browserHasGeolocation, infoWindow, pos) {
+  infoWindow.setPosition(pos);
+  infoWindow.setContent(browserHasGeolocation ?
+                        'Error: The Geolocation service failed.' :
+                        'Error: Your browser doesn\'t support geolocation.');
+}
+
+function drop() {
+  clearMarkers();
+  for (var i = 0; i < neighborhoods.length; i++) {
+    addMarkerWithTimeout(neighborhoods[i], i * 200);
+  }
+}
+
+function addMarkerWithTimeout(position, timeout) {
+  window.setTimeout(function() {
+    markers.push(new google.maps.Marker({
+      position: position,
+      map: map,
+      animation: google.maps.Animation.DROP
+    }));
+  }, timeout);
+}
+
+// Clear all the markers
+function clearMarkers() {
+  for (var i = 0; i < markers.length; i++) {
+    markers[i].setMap(null);
+  }
+  markers = [];
+}
+
+// google.maps.event.addDomListener(window, 'load', initMap);
